a617ec28337509702610e494cd0cd48d8efb0006
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-10-08 14:39:06 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    def initialize(booktitle)
+        @title = booktitle
+    end
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end 
 
 
+Book.new("And Then There Were None")
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
 # Make your shoe class here!
+class Shoe 
+    attr_reader :brand 
+    attr_accessor :color, :size, :material, :condition
+    def initialize(shoe_brand)
+        @brand = shoe_brand
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end 
+end
