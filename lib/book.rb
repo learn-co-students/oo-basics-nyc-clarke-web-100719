61ccb90992e7e4f93c12736c978425e7b837ba37
soufianeou <soufiane.oucherrou@gmail.com>
2019-10-08 17:44:21 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,42 @@
+class Book
+ attr_accessor :author, :page_count
+    def initialize(name)
+        @name = name
+    end
+    def title
+        @name
+    end
+ 
+ # def author=(author_name)
+ #     @author_name = author_name
+ #     @author_name
+ # end
+ # def author
+ #     @author_name
+ # end
 
+#  def page_count=(pages)
+#      @pages = pages
+#  end
 
+#  def page_count
+#      @pages
+#  end
+
+def genre=(arg)
+    @genre = arg
+end
+def genre
+    @genre
+end
+
+def turn_page
+    puts "Flipping the page...wow, you read fast!"
+end
+
+end
+
+book1 = Book.new("And Then There Were None")
+book1.author = "Agatha Christie"
+book1.page_count = 272
+book1.genre = "Mystry"
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,38 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :condition
+    
+    def initialize(brand)
+        @brand = brand
+    end
+
+   def color=(color)
+      @color = color 
+   end
+   def color
+       @color
+   end
+    def size=(size)
+       @size = size
+   end
+   def size
+       @size
+   end
+   def material=(material)
+       @material = material
+   end
+    def material
+        @material
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+first_shoe = Shoe.new("Adidas")
+first_shoe.brand = "Nike"
+first_shoe.color = "red"
+first_shoe.size = 9.5
+first_shoe.material = "sued"
+first_shoe.condition = "old"
